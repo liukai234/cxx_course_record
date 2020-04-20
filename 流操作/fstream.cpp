04e86ff45d0030894899ab1f13d3349136d8a459
liukai234 <2679327337@qq.com>
2020-04-20 10:40:07 +0800

    add /transEmp十进制文本格式转换

--- 流操作/fstream.cpp
+++ 流操作/fstream.cpp
@@ -2,7 +2,7 @@
  * @Description: 文件流
  * @LastEditors: liukai
  * @Date: 2020-04-17 14:57:20
- * @LastEditTime: 2020-04-18 16:54:56
+ * @LastEditTime: 2020-04-20 09:07:54
  * @FilePath: /C++课程学习记录/流操作/fstream.cpp
  */
 #include <fstream>
---
+++ 流操作/transEmp.cpp
@@ -0,0 +1,38 @@
+/*
+ * @Description: format zen of python
+ * @LastEditors: liukai
+ * @Date: 2020-04-20 10:10:54
+ * @LastEditTime: 2020-04-20 10:38:52
+ * @FilePath: /C++课程学习记录/流操作/transEmp.cpp
+ */
+#include <fstream>
+#include <iostream>
+#include <string>
+// #include <strstream>
+using namespace std;
+
+int main() {
+    char ch[200];
+    string str;
+    ifstream infile("input.txt", ios::in);
+    if(!infile) { cerr << "open error" << endl; exit(1); } // cerr 不会计入缓冲区
+
+    ofstream outfile("output.txt", ios::out);
+    if(!outfile) { cerr << "open error" << endl; exit(1); }
+    
+    while(infile.peek() != EOF){
+        infile.getline(ch, 200);
+        outfile << "<font color=\"#e74c3c\">"; 
+        for(int i = 0; ch[i] != 0; i++){
+            if(ch[i] == '(') outfile << "//"; // do something 借助宽字符模式
+            else if(ch[i] == ')') { /* nothing */ }
+            else outfile << ch[i];
+            if(ch[i] == '.') outfile << "</font>&nbsp;&nbsp;"; 
+        }
+        outfile << endl;
+    }
+    
+    infile.close();
+    outfile.close();
+    return 0;
+}
