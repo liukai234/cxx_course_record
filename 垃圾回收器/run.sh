72dce935688c47eb6592f5099a5445f86afad75c
liukai234 <2679327337@qq.com>
2020-10-25 03:30:53 +0000

    update run.sh

--- 垃圾回收器/run.sh
+++ 垃圾回收器/run.sh
@@ -3,11 +3,17 @@
 printf "Shell Script running...\n" && 
 
 g++ main.cpp ManageAllocation.cpp Object.cpp -o test -std=c++14 &&
-mkdir out &&
+
+# 空格必须存在
+if [ ! -d out ]; then
+    mkdir out
+fi
+
 mv test out/test &&
 out/test &&
 
-printf "Shell Script end\n"
+ret=$? # $? 显示最后命令的退出状态
+printf "Program return $ret\n" 
 
 # chmod u+x file # 赋予user执行权限 drwxrwxrwx
 # d 表示目录
