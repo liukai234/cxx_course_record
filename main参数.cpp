088947ea4290b6e2814aae7d793b8e464e4d752a
liukai <liukai8233@outlook.com>
2021-04-03 21:05:57 +0800

    add: char ** argv 和 string 转换

--- main参数.cpp
+++ main参数.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main(int argc, char **argv) {
+    /* char*与string风格互相转换时使用c_str和用string新建对象 */
     vector<string> v1;
     for(int i = 1; i < argc; i++) {
         v1.push_back(string(argv[i]));
