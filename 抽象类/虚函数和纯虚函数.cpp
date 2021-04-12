550d079afc743de8deaeb32560c570a162477eee
liukai <liukai8233@outlook.com>
2021-04-12 08:31:05 +0800

    feat: 虚函数和纯虚函数的意义

--- 抽象类/虚函数和纯虚函数.cpp
+++ 抽象类/虚函数和纯虚函数.cpp
@@ -7,7 +7,9 @@ public:
     Base() = default;
     Base(int a, int b) {}
     // 虚函数必须拥有方法体，带有虚函数的类可以定义对象
-    virtual void add(int a, int b) { cout << a + b << endl; }
+    // 定义他为虚函数是为了允许用基类的指针来调用子类的这个函数，实现多态
+    // 但子类不必须重载虚函数
+    virtual void add(int a, int b) { cout << "Base: " << a + b << endl; }
 };
 
 class Dervied : public Base {
@@ -22,6 +24,8 @@ class VD : public Base {
 public:
     VD():Base(){}
     // 虚函数 =0 则成为纯虚函数，含有纯虚函数的类称为抽象类，抽象类不能定义对象
+    // 定义纯虚函数是为了实现一个接口，起到一个规范的作用，规范继承这个类的程序员必须实现这个函数
+    // 在基类中的这个虚函数含有方法体时其是无意义的
     virtual void add(int a, int b) = 0;
 };
 
