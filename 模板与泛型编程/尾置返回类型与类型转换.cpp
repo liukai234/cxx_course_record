c6d979a6800c99adb98711a1bc50fbe0047aa1c7
liukai <liukai8233@outlook.com>
2021-04-21 18:53:51 +0800

    feat: 脱引用

--- 模板与泛型编程/尾置返回类型与类型转换.cpp
+++ 模板与泛型编程/尾置返回类型与类型转换.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 
+// 标准库中的类型转换模板
+#include <type_traits>
+
 template<class T>
 class Base {
 public:
     explicit Base(int a):a_(a){}
     static T b_;
-    // decltype(a_) 中的a_来自于此函数的参数
-    auto a(int a_) -> decltype(a_) { return a_; }
+    // decltype(ret) 中的ret来自于此函数ret的参数
+    // 脱引用操作 [type_traits] std::remove_reference<decltype(ret)>::type 
+    // 如果 ret 返回 *beg 的话，返回的结果是解引用的，解引用运算符将返回左值，因此是一个类型的引用
+    // 如：string 返回 string&，int 返回 int&
+    auto ret(int ret) -> /*decltype(ret)*/ typename std::remove_reference<decltype(ret)>::type { return ret; }
 private:
     T a_;
 };
-
-
 
 template<class T>
 T Base<T>::b_ = 3;
@@ -19,5 +23,7 @@ T Base<T>::b_ = 3;
 int main() {
     Base<int> base(0);
     using baseInt = Base<int>;
-    std::cout << baseInt::b_;
+    std::cout << baseInt::b_ << "\n";
+    int a = base.ret(5);
+    std::cout << a << "\n";
 }
