bc0d9718f55eb7d4953c06bd700e084baf413503
liukai <liukai8233@outlook.com>
2021-04-24 12:27:54 +0800

    feat: sstream 输出到 istream

--- 流操作/继承关系.cpp
+++ 流操作/继承关系.cpp
@@ -2,3 +2,15 @@
 #include <sstream>     // sstream istringstream ostringstream
 #include <fstream>     // fstream ifstream ofstream 
 // fstream 和 sstream 都继承自 iostream
+
+using namespace std;
+
+int main() {
+    stringstream ss("12306");
+    int a;
+    istream is(ss.rdbuf());
+    // or ss >> a;
+    is >> a;
+    cout << a;
+
+}
