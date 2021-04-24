2903c47b2bd2afd6915dbf2af9ec241b83282112
liukai <liukai8233@outlook.com>
2021-04-24 12:15:19 +0800

    feat: 流继承关系

---
+++ 流操作/继承关系.cpp
@@ -0,0 +1,4 @@
+#include <iostream>    // iostream ostream istream 
+#include <sstream>     // sstream istringstream ostringstream
+#include <fstream>     // fstream ifstream ofstream 
+// fstream 和 sstream 都继承自 iostream
