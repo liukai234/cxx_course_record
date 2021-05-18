31b21acfb17077e4a225374c82dace6ef8d28961
liukai <liukai8233@outlook.com>
2021-05-18 19:23:20 +0800

    fix: 增加函数体

--- 模板与泛型编程/函数模板.cpp
+++ 模板与泛型编程/函数模板.cpp
@@ -1,15 +1,25 @@
 // ../现代C++特性/函数指针.cpp
 // 函数模板
+// 函数指针
 #include <iostream>
 
 using namespace std;
 
-template <typename T> int compare(const T&, const T&);
+template <typename T> int compare(const T&, const T&) {
+    return 0;
+}
 
-void func(int (*) (const int &, const int &));
-void func(int (*) (const string &, const string &));
+void func(int (*) (const int &, const int &)) {
+    // dosomething
+}
+void func(int (*) (const string &, const string &)) {
+    // dosomething
+}
+
+// pf1 中参数的类型决定了T的模板实参的类型，如果不能从函数指针类型确定模板参数则产生错误
+int (*pf1) (const int &, const int &) = compare;
 
 int main() {
-    int (*pf1) (const int &, const int &) = compare; // 定义函数指针pf并置空
     func(compare<int>);
+    func(pf1);
 }
