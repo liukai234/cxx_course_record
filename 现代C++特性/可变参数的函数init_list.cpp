90a8429a1cabc8384d9408549fb1311e10c6e919
liukai <liukai8233@outlook.com>
2021-07-10 22:31:41 +0800

    update init_list

--- 现代C++特性/可变参数的函数init_list.cpp
+++ 现代C++特性/可变参数的函数init_list.cpp
@@ -12,12 +12,13 @@ using std::exception;
 using std::initializer_list;
 
 // 与vector不同的是init_list中的值永远是常量值
-int func(initializer_list<string> lst) {
+int func(int a, initializer_list<string> lst) {
     lst.size();
+    cout << a << endl;
     for(auto x : lst) { cout << x << endl; }
     return 0;
 }
 
 int main() {
-    func({"123", "456", "789"});
+    func(10, {"123", "456", "789"});
 }
