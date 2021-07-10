9e052c611907f2e5a908684e73c61b7736519e2e
liukai <liukai8233@outlook.com>
2021-07-10 11:35:20 +0800

    fix: const的引用

--- const常量/const的引用.cpp
+++ const常量/const的引用.cpp
@@ -1,9 +1,13 @@
-// 引用类型必须与其所引用对象的类型一致
-// 例外：1. 初始化常量引用时允许用任意表达式作为初始值 2，对const引用一个非const的对象
+// 引用类型必须与其所引用对象的类型一致，普通引用只能绑定在对象上
+// 例外：1. 初始化常量引用（const）时允许用任意表达式作为初始值 2，对const引用一个非const的对象
 
 int main() {
+    // int &i = 2; // error
     const int &b = 10 + 6; // 例外1
+    const int &c = b;
+
     int a = 2;
+    // int & &d = a; // error 引用的引用
     const int &aa = a; // 例外2
     a = 3;
     // aa = 4; // error
